6910d1a0049c980bc5a803ff411c3d75fef711c0
Wojciech Pstrzoch <wojciech.pstrzoch@omegacode.pl>
2017-07-20 13:13:09 +0200

    edit sort grid

--- zadanie/zadanie/Controllers/ProductsController.cs
+++ zadanie/zadanie/Controllers/ProductsController.cs
@@ -3,62 +3,83 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Threading.Tasks;
 using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using zadanie.Models;
 using PagedList;
+using Microsoft.Practices.Unity;
 using System.Linq.Dynamic;
+using zadanie.ViewModels;
 
 namespace zadanie.Controllers
 {
     public class ProductsController : Controller
     {
-        private NorthwindEntities1 db = new NorthwindEntities1();
+        private NorthwindEntities1 db;
         
-        // GET: Products
-        public async Task<ActionResult> Index()
+        public ProductsController(NorthwindEntities1 db)
         {
-            var products = from p in (db.Products.OrderBy(p => p.ProductName).Include(p => p.Categories).Include(p => p.Suppliers)) select p;
-           
-            return View(await products.ToListAsync());
+            this.db = db;
+        }
+        // private NorthwindEntities1 db = new NorthwindEntities1();
+
+        // GET: Products1
+        public ActionResult Index(ProductViewModel model)
+        {
+            // Get Products
+            model.Products = db.Products
+                .Where(
+                    x =>
+                    (model.ProductName == null || x.ProductName.Contains(model.ProductName)))
+                .OrderBy(model.Sort + " " + model.SortDir)
+                .Skip((model.Page - 1) * model.PageSize)
+                .Take(model.PageSize)
+                .ToList();
+
+            // total records for paging
+            model.TotalRecords = db.Products
+                .Count(x =>
+                    (model.ProductName == null || x.ProductName.Contains(model.ProductName))
+                    );
+
+            return View(model);
         }
 
-        // GET: Products/Details/5
-        public async Task<ActionResult> Details(int? id)
+        // GET: Products1/Details/5
+        public ActionResult Details(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Products products = await db.Products.FindAsync(id);
+            Products products = db.Products.Find(id);
             if (products == null)
             {
                 return HttpNotFound();
             }
-            return View(products);
+            return PartialView("Details", products);
         }
 
-        // GET: Products/Create
+        // GET: Products1/Create
         public ActionResult Create()
         {
             ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
             ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName");
-            return View();
+            return PartialView("Create");
         }
 
-        // POST: Products/Create
+        // POST: Products1/Create
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Create([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
+        public ActionResult Create([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
         {
             if (ModelState.IsValid)
             {
                 db.Products.Add(products);
-                await db.SaveChangesAsync();
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
 
@@ -67,34 +88,34 @@ namespace zadanie.Controllers
             return View(products);
         }
 
-        // GET: Products/Edit/5
-        public async Task<ActionResult> Edit(int? id)
+        // GET: Products1/Edit/5
+        public ActionResult Edit(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Products products = await db.Products.FindAsync(id);
+            Products products = db.Products.Find(id);
             if (products == null)
             {
                 return HttpNotFound();
             }
             ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
             ViewBag.SupplierID = new SelectList(db.Suppliers, "SupplierID", "CompanyName", products.SupplierID);
-            return View(products);
+            return PartialView("Edit", products);
         }
 
-        // POST: Products/Edit/5
+        // POST: Products1/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> Edit([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
+        public ActionResult Edit([Bind(Include = "ProductID,ProductName,SupplierID,CategoryID,QuantityPerUnit,UnitPrice,UnitsInStock,UnitsOnOrder,ReorderLevel,Discontinued")] Products products)
         {
             if (ModelState.IsValid)
             {
                 db.Entry(products).State = EntityState.Modified;
-                await db.SaveChangesAsync();
+                db.SaveChanges();
                 return RedirectToAction("Index");
             }
             ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", products.CategoryID);
@@ -102,29 +123,29 @@ namespace zadanie.Controllers
             return View(products);
         }
 
-        // GET: Products/Delete/5
-        public async Task<ActionResult> Delete(int? id)
+        // GET: Products1/Delete/5
+        public ActionResult Delete(int? id)
         {
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
             }
-            Products products = await db.Products.FindAsync(id);
+            Products products = db.Products.Find(id);
             if (products == null)
             {
                 return HttpNotFound();
             }
-            return View(products);
+            return PartialView("Delete", products);
         }
 
-        // POST: Products/Delete/5
+        // POST: Products1/Delete/5
         [HttpPost, ActionName("Delete")]
         [ValidateAntiForgeryToken]
-        public async Task<ActionResult> DeleteConfirmed(int id)
+        public ActionResult DeleteConfirmed(int id)
         {
-            Products products = await db.Products.FindAsync(id);
+            Products products = db.Products.Find(id);
             db.Products.Remove(products);
-            await db.SaveChangesAsync();
+            db.SaveChanges();
             return RedirectToAction("Index");
         }
 
@@ -136,27 +157,26 @@ namespace zadanie.Controllers
             }
             base.Dispose(disposing);
         }
-        [HttpPost]
-        public ActionResult LoadData()
+        public ActionResult List(string sortOrder, int? Page_No)
         {
+            var products = from p in (db.Products.OrderBy(p => p.ProductName).Include(p => p.Categories).Include(p => p.Suppliers)) select p;
 
-            var draw = Request.Form.GetValues("draw").FirstOrDefault();
-            var start = Request.Form.GetValues("start").FirstOrDefault();
-            var length = Request.Form.GetValues("length").FirstOrDefault();
-
-
-            int pageSize = length != null ? Convert.ToInt32(length) : 0;
-            int skip = start != null ? Convert.ToInt32(start) : 0;
-            int recordsTotal = 0;
-            using (NorthwindEntities1 db = new NorthwindEntities1())
+            if (!String.IsNullOrWhiteSpace(sortOrder))
             {
-                // dc.Configuration.LazyLoadingEnabled = false; // if your table is relational, contain foreign key
-                var products = from p in (db.Products.OrderBy(p => p.ProductName).Include(p => p.Categories).Include(p => p.Suppliers)) select p;
-
-                recordsTotal = products.Count();
-                var data = products.Skip(skip).Take(pageSize).ToList();
-                return Json(new { draw = draw, recordsFiltered = recordsTotal, recordsTotal = recordsTotal, data = data }, JsonRequestBehavior.AllowGet);
+                if (sortOrder == "ProductName_ASC")
+                    products = products.OrderBy(c => c.ProductName);
+                else if (sortOrder == "ProductName_DESC")
+                    products = products.OrderByDescending(c => c.ProductName);
+                
             }
+            else
+            {
+                products = products.OrderBy(c => c.ProductName);
+            }
+
+            int Size_Of_Page = 10;
+            int No_Of_Page = (Page_No ?? 1);
+            return View(products.ToPagedList(No_Of_Page, Size_Of_Page));
         }
     }
 }
